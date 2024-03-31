2734595075a678ef3a6988bfffeb4e8f5bcd2772
AksilDev <axcelsonmacansantos@gmail.com>
2024-04-01 02:48:41 +0800

    noui changed

--- noui.js
+++ noui.js
@@ -1,3 +1,5 @@
+// works basta website online javaS compiler
+//
 function calcu(num1, num2, op) {
     switch (op) {
         case '+':
@@ -10,17 +12,17 @@ function calcu(num1, num2, op) {
             if (num2 !== 0) {
                 return num1 / num2;
             } else {
-                return "Error: Division by zero!";
+                return "Error in da Division";
             }
         default:
-            return "Error: Invalid operator!";
+            return "Error Invalid opertor";
     }
 }
 
 function main() {
-    var num1 = parseFloat(prompt("Enter first int: "));
-    var num2 = parseFloat(prompt("Enter second ints: "));
-    var op = prompt("Enter the operator (+, -, *, /): ");
+    var num1 = parseFloat(prompt("Enter first : "));
+    var num2 = parseFloat(prompt("Enter seconds : "));
+    var op = prompt("Enter operator (+, -, *, /): ");
 
     console.log(calcu(num1, num2, op));
 }
